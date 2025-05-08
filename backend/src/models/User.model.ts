9e9939fcc60f9fe09d79b009eb03c59cd1b4c25d
Asaiy420 <kcaryan05@gmail.com>
2025-05-08 20:30:36 +0545

    changed structure of the model

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    username: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
@@ -13,10 +17,6 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
 
-    name: {
-      type: String,
-      required: true,
-    },
     lastLogin: {
       type: Date,
       default: Date.now,
