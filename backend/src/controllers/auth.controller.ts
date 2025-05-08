57f24a3a7d85248d4700135e8e270e1586bdef71
Asaiy420 <kcaryan05@gmail.com>
2025-05-08 21:21:59 +0545

    fixed typos and endpoints and added auth controllers

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,14 +1,73 @@
-import express, {Request, Response} from "express"
-
+import { Request, Response } from "express";
+import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 
 export const Signup = async (req: Request, res: Response): Promise<any> => {
-    res.send("Signup Page")
-}
+  const { username, email, password } = req.body;
+
+  // Check for empty fields
+  try {
+    if (!username || !email || !password) {
+      throw new Error("All fields are required");
+    }
+
+    // check if user already exists
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    // hash the password
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // get the verification code
+
+    const verificationToken = generateVerificationToken();
+
+    // create the new user
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      verificationToken,
+
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    // save the user to db
+
+    await user.save();
+
+    // jwt
+
+    generateTokenAndSetCookie(res, user._id);
+
+    const userObj = user.toObject() as any;
+        delete userObj.password;
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      user: userObj
+    });
+  } catch (error: any) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const Login = async (req: Request, res: Response): Promise<any> => {
-    res.send("Login page")
-}
+  res.send("Login page");
+};
 
 export const Logout = async (req: Request, res: Response): Promise<any> => {
-    res.send("logout page")
-}
+  res.send("logout page");
+};
--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
-    isVerfied: {
+    isVerified: {
       type: Boolean,
       default: false,
     },
